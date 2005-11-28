575e9c499920f538d7a837cbc42607252f135b6f
Ross Bamford <roscopeco@gmail.com>
2005-11-28 14:40:40 +0000

    Added user guide Documented documentation as the example site, too Updated Readme and added TODOs

--- doc/pages/COMMON.rb
+++ doc/pages/COMMON.rb
@@ -1,3 +1,14 @@
+# Common code for every page.
+#
+# Instead of being executed once (as you might expect), this is evaluated
+# for each Page's binding as the instance is created.
+#
+# At the moment, COMMON.rb code isn't automatically inherited from
+# parent directories - you have to use the 'inherit_common' hack provided
+# by Page to inherit from the immediate parent directory ONLY (setting
+# up chains of COMMONs if you have deep nesting - Ugh!). This limitation
+# will be removed very soon.
+
 @site_title = 'Rote'
 @base_url = 'http://rote.rubyforge.org/'
 
@@ -5,16 +16,23 @@
 layout 'page'
 format_opts << :textile
 
-# this is used to construct the navbar and frontpage.
+# this is used to construct the navbar and frontpage. Note that we use absolute
+# (root-relative) URLs here, and fix them from each page with 'link_rel'.
 @navigation = [
 					{:title => 'Home',
 					 :url => '/index.html'},
 					{:title => 'Download',
 					 :url => 'http://rubyforge.org/frs/?group_id=1120'},					 
+					{:title => 'User guide',
+					 :url => '/guide/'},					 
 					{:title => 'RDoc',
-					 :url => 'rdoc/'},					 
+					 :url => '/rdoc/'},
+					{:title => 'Issue tracker',
+					 :url => 'http://rubyforge.org/tracker/?group_id=1120'},
 					{:title => 'Project home',
 					 :url => 'http://rubyforge.org/projects/rote'},					 
 					{:title => 'Browse CVS',
-					 :url => 'http://rubyforge.org/cgi-bin/viewcvs.cgi/?cvsroot=rote'}					 
+					 :url => 'http://rubyforge.org/cgi-bin/viewcvs.cgi/?cvsroot=rote'},
+					{:title => 'License',
+					 :url => '/license.html'}					 
 					]
---
+++ doc/pages/guide/COMMON.rb
@@ -0,0 +1,7 @@
+# This is a hack that exists currently to inherit COMMON.rb
+# from above. Of course, we can still define extra stuff
+# here. 
+#
+# This will be made implicit soon.
+
+inherit_common
