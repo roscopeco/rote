9dc5f7ae4b933ed34d1c83a2a9331a84c2e31795
Ross Bamford <roscopeco@gmail.com>
2005-11-28 14:40:40 +0000

    Added user guide Documented documentation as the example site, too Updated Readme and added TODOs

--- rote/doc/pages/index.rb
+++ rote/doc/pages/index.rb
@@ -1,4 +1,8 @@
-# the default
-#layout 'page'
-
+# Page code for 'index.html'
+#
+# In practice, for something this simple you'd usually go with setting
+# the var directly in the template, e.g.
+#
+#   <% @page_title = 'Welcome!' %>
+#
 @page_title = 'Welcome!'
