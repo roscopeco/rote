de622ec238499481f256cd11affd9af4b6db9b51
Ross Bamford <roscopeco@gmail.com>
2005-12-01 11:08:31 +0000

    Updated to version 0.2.0

--- lib/rote.rb
+++ lib/rote.rb
@@ -41,7 +41,7 @@ require 'net/ftp'
 require 'rake'
 
 # Master Rote version. Manage this from the Rake release support.
-ROTEVERSION = '0.1.7'
+ROTEVERSION = '0.2.0'
 
 #####
 ## *Rote* is a Rake (http://rake.rubyforge.org) based build tool for page-based
