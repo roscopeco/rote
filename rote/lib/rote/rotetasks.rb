fa60c328ccd4186e4db7730de4669d9a15acf976
Ross Bamford <roscopeco@gmail.com>
2005-12-02 19:55:03 +0000

    Suggested by Jonathan Paisley:   * New 'monitor' task and support   * Update-check on resources   * New 'clean' task
    Also
      * Deprecated 'show_page_tasks' (use 'show_file_tasks instead')
      * Fixed ugly page file task implementation

--- rote/lib/rote/rotetasks.rb
+++ rote/lib/rote/rotetasks.rb
@@ -82,12 +82,24 @@ module Rote
     # Globs for the +FileList+ that supplies the resources to copy. You 
     # should configure the +layout_dir+ and +include+ at least one entry
     # here (you may add +exclude+ strings or regexps, too).
+    #
+    # This is *not* a +FileList+ - the patterns supplied to this are used
+    # with the base-directory specified to construct an appropriate
+    # +FileList+.
     attr_reader :res
     
     # If +show_page_tasks+ is +true+, then the file tasks created for each
     # source page will be shown in the Rake task listing from the command line.
-    attr_accessor :show_page_tasks         
-    alias :show_page_tasks? :show_page_tasks
+    attr_accessor :show_file_tasks         
+    alias :show_file_tasks? :show_file_tasks
+    
+    # This is a *deprecated* alias for +show_file_tasks+. It will be removed.
+    alias :show_page_tasks? :show_file_tasks      # vv0.3.0 v-0.5
+    alias :show_page_tasks= :show_file_tasks=
+    
+    # The approximate number of seconds between update checks when running
+    # monitor mode (Default: 1)
+    attr_accessor :monitor_interval    
     
     # Create a new DocTask, using the supplied block for configuration,
     # and define tasks with the specified base-name within Rake.
@@ -96,6 +108,7 @@ module Rote
       @output_dir = '.'      
       @pages = FilePatterns.new('.')
       @res = FilePatterns.new('.')
+      @monitor_interval = 1
       DEFAULT_SRC_EXCLUDES.each { |excl| @pages.exclude(excl) }
       
       @show_page_tasks = false
@@ -114,55 +127,89 @@ module Rote
       nil
     end
     
+    # define a task for each resource, and 'all resources' task
     def define_res_tasks
       res_fl = res.to_filelist
+      tasks = res_fl.select { |fn| not File.directory?(fn) }.map do |fn|
+        tfn = fn.sub(/^#{res.dir}/, output_dir)
+        desc "#{fn} => #{tfn}" if show_file_tasks?
+        file tfn => [fn] do
+          dn = File.dirname(tfn)
+          mkdir_p dn unless File.exists?(dn)
+          cp fn, tfn
+        end
+        tfn
+      end
       
-      desc "Copy documentation resources"
-      task "#{name}-res" do
-        res_fl.each { |fn|           
-          unless File.directory?(fn)    # make dirs only as needed
-            tfn = fn.sub(/#{res.dir}/, output_dir)
-            dn = File.dirname(tfn)
-            mkdir_p dn unless File.exists?(dn)            
-            cp fn, tfn
-          end        
-        }
-      end #task
+      task "#{name}-res" => tasks     
     end
 
+    # define a task for each page, and 'all pages' task
     def define_page_tasks
-      # make file list
-      pages_fl = pages.to_filelist
-    
-      # define a task for each page
-      realpages = FileList[]
-      pages_fl.each { |fn| 
-        unless File.directory?(fn)    # make dirs only as needed
-          realpages << fn
-          tfn = fn.sub(/^#{pages.dir}/, output_dir)
-          
-          desc "#{fn} => #{tfn}" if show_page_tasks?
-          file tfn => [fn] do
-            dn = File.dirname(tfn)
-            mkdir_p dn unless File.exists?(dn)
-            File.open(tfn, 'w+') { |f|
-              puts "tr #{fn} => #{tfn}"
-              f << Page.new(fn,layout_dir).render
-            }
-          end
+      pages_fl = pages.to_filelist    
+      tasks = pages_fl.select { |fn| not File.directory?(fn) }.map do |fn| 
+        tfn = fn.sub(/^#{pages.dir}/, output_dir)          
+        desc "#{fn} => #{tfn}" if show_file_tasks?
+        file tfn => [fn] do
+          dn = File.dirname(tfn)
+          mkdir_p dn unless File.exists?(dn)
+          File.open(tfn, 'w+') do |f|
+            puts "tr #{fn} => #{tfn}"
+            f << Page.new(fn,layout_dir).render
+          end          
         end
-      }
+        tfn
+      end
       
-      # this is pretty convenient ;]
       desc "Render all documentation pages"
-      task "#{name}-pages" => realpages.sub(/#{pages.dir}/, output_dir)
+      task "#{name}-pages" => tasks
     end
     
     def define_main_tasks
       desc "Build the documentation"
       task name => ["#{name}-pages", "#{name}-res"]
+
+      desc "Remove the generated documentation"
+      task :clean => [ "#{name}-clean" ]
+      task "#{name}-clean" do
+        rm_rf output_dir
+      end    
+      
+      # thanks to Jonathan Paisley for this :)    
+      # Relies on Rake mods made below.
+      desc "Monitor and automatically rebuild the documentation"
+      task "#{name}-monitor" do
+        loop do
+          Rake::Task::tasks.each { |t| t.reset }
+          Rake::Task[name].invoke
+          if Rake::Task::tasks.grep(Rake::FileTask).detect { |t| t.executed? } then
+            Rake::Task["#{name}-refresh"].invoke if Rake::Task.task_defined?("#{name}-refresh")
+          end
+          sleep monitor_interval
+        end
+      end        
+      
     end
     
   end #class  
   
 end #module
+
+## The -run task requires a few mods to Rake to let us fire
+## and reset task invocations in a loop.
+module Rake
+  class Task
+    def reset
+      @already_invoked = false
+      @executed = false
+    end
+    def executed?
+      @executed
+    end
+    alias :pre_rote_execute :execute
+    def execute
+      @executed = true
+      pre_rote_execute
+    end
+  end
+end       
