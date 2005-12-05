6c0a72cd3fad6b256a7494e3d64ac8d41d1d580b
Ross Bamford <roscopeco@gmail.com>
2005-12-05 14:43:22 +0000

    Added standard project template for 'create' task

---
+++ lib/rote/project/doc/pages/COMMON.rb
@@ -0,0 +1,7 @@
+# Shared code for all pages in this directory.
+
+# This is executed for every file transformed, in the binding of 
+# the appropriate Rote::Page instance. Individual page sources
+# ([pagename].rb) override anything defined here.
+
+@site_title = "My site"
---
+++ lib/rote/project/doc/pages/index.rb
@@ -0,0 +1,13 @@
+# Page code for the 'index' page. We call a few methods to
+# set layout and formatting, and then create a custom variable
+# for use in the layout.
+
+# This page uses Textile
+format_opts << :textile
+
+# Apply the 'normal' layout
+layout 'normal'
+
+# Set a custom var for use in our layout
+@page_title = 'A sample page'
+        
