2579989dfda0f7bef3875fed56b4c1648fa5e1c0
Ross Bamford <roscopeco@gmail.com>
2005-12-05 14:46:43 +0000

    Guide updated with new startup stuff Dropped back to transitional xhtml until guide validates strict

--- doc/pages/guide/COMMON.rb
+++ doc/pages/guide/COMMON.rb
@@ -12,7 +12,7 @@ end
 def section(level, name, toplink = true)
 %Q{
 #{"[#{section_link('Top')}]" if toplink}
-<a name='#{section_anchor(name)}'/>
+<a name='#{section_anchor(name)}'></a>
 h#{level}. #{name}
 }
 end
