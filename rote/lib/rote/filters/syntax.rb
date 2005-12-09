10967192a6036022e7c09742acb1043882499cff
Ross Bamford <roscopeco@gmail.com>
2005-12-09 11:02:07 +0000

    Support customisable code matching

--- rote/lib/rote/filters/syntax.rb
+++ rote/lib/rote/filters/syntax.rb
@@ -24,10 +24,19 @@ module Rote
     ##   {code}
     ##  
     class Syntax
-      CODE_RE = /\{code(?:\:?(\w+))?\}(.*?)\{\/?code\}/m
+      DEFAULT_CODE_RE = /\{code(?:\:?(\w+))?\}(.*?)\{\/?code\}/m
+      
+      # Create a new syntax highlight filter that uses the specified regular
+      # expression to recognise and process code blocks.
+      # The expression supplied must have two capturing groups, the
+      # first returning the code language (i.e. 'ruby') and the second
+      # returning the actual code.
+      def initialize(code_re = DEFAULT_CODE_RE)
+        @code_re = code_re
+      end
       
       def filter(text, page)        
-        text.gsub(CODE_RE) do
+        text.gsub(@code_re) do
           converter = ::Syntax::Convertors::HTML.for_syntax($1)
           "<pre><code>#{converter.convert($2,false)}</code></pre>"
         end
