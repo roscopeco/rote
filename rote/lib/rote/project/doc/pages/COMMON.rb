735d8272407ebb5bc7eee6c4a2e6c5982a7b9cb2
Ross Bamford <roscopeco@gmail.com>
2005-12-14 18:30:09 +0000

    Fixed standard page-code bug

--- rote/lib/rote/project/doc/pages/COMMON.rb
+++ rote/lib/rote/project/doc/pages/COMMON.rb
@@ -1,7 +1,9 @@
-# Shared code for all pages in this directory.
-
+# Shared code for all pages below this directory.
+#
 # This is executed for every file transformed, in the binding of 
 # the appropriate Rote::Page instance. Individual page sources
 # ([pagename].rb) override anything defined here.
-
+#
+# Any COMMON.rb files found in directories above this will also
+# be loaded.
 @site_title = "My site"
