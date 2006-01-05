62bf217303bd3542967b0667541ec3df58762285
Ross Bamford <roscopeco@gmail.com>
2006-01-05 21:15:54 +0000

    Merged 0.3.2

--- rote/lib/rote/filters.rb
+++ rote/lib/rote/filters.rb
@@ -1,10 +1,26 @@
 #--
 # Require all filters
-# (c)2005 Ross Bamford (and contributors)
+# (c)2005, 2006 Ross Bamford (and contributors)
 #
 # See 'rote.rb' or LICENSE for licence information.
 # $Id$
 #++
 
-# will require 'base' but it'd get required anyway...
-Dir[File.join(File.dirname(__FILE__), 'filters/*.rb')].each { |f| require f }
+# Everyone requires this, we need to get it loaded first.
+require 'rote/filters/base'
+
+# This now requires conservatively, ignoring any filters that
+# throw a LoadError. This allows a simple 'require rote/filters' to
+# be used to load all filters _for which dependencies are
+# available_.
+Dir[File.join(File.dirname(__FILE__), 'filters/*.rb')].each do |fn|
+  begin
+    require fn unless fn =~ /[\\\/]base.rb$/
+  rescue LoadError
+
+    # ignore, different filters require different library
+    # support.
+
+  end
+end
+
